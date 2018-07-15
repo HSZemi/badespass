201775fbb69e2bca7b7f04d8b530c322c91ce0e4
HSZemi <hszemi@users.noreply.github.com>
2018-07-15 22:08:56 +0200

    Add badespass

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # badespass
 Bonn bleibt Spaßfrei!
+
+
+## Images
+
+* [water-1018808_1920.jpg](https://pixabay.com/de/wasser-schwimmbad-baden-reisen-1018808/)
+* [sculpture-3071949_1920.jpg](https://pixabay.com/de/skulptur-frau-figur-statue-kunst-3071949/)
+* [lost-places-1510592_1920.jpg](https://pixabay.com/de/lost-places-fabrik-alt-verfallen-1510592/)
+* [swim-422546_1920.jpg](https://pixabay.com/de/schwimmen-wasser-tauchen-422546/)
+* [euro-1353420_1920.jpg](https://pixabay.com/de/euro-m%C3%BCnzen-w%C3%A4hrung-geld-gelb-1353420/)
+* [lost-places-1495150_1920.jpg](https://pixabay.com/de/lost-places-fabrik-halle-verlassen-1495150/)
+* [swimming-659903_1920.jpg](https://pixabay.com/de/schwimmen-wettbewerb-schwimmer-659903/)
+* [ruin-636381_1920.jpg](https://pixabay.com/de/ruine-halle-verfallen-verfall-636381/)
+* [swimming-pool-816394_1920.jpg](https://pixabay.com/de/schwimmbad-wasser-sommer-schwimmen-816394/)
+* [swimmers-79592_1920.jpg](https://pixabay.com/de/schwimmer-schwimmen-rennen-79592/)
+* [swim-ring-84625_1920.jpg](https://pixabay.com/de/schwimmen-ring-wasser-schwimmbad-84625/)
+* [ruin-1563208_1920.jpg](https://pixabay.com/de/ruine-verfallen-verfall-verlassen-1563208/)
+* [baron-munchausen-74043_1920.jpg](https://pixabay.com/de/baron-m%C3%BCnchhausen-l%C3%BCgengeschichten-74043/)
+* [town-home-3352209_1920.jpg](https://pixabay.com/de/stadthaus-bonn-glas-reflexion-3352209/)
+* [bonn-209505_1920.jpg](https://pixabay.com/de/bonn-nordrhein-westfalen-poppelsdorf-209505/)
+* [skeletons-1617539_1920.jpg](https://pixabay.com/de/skelette-lustig-h%C3%B6re-nichts-b%C3%B6ses-1617539/)
+* [thumbs-up-2056022_1920.jpg](https://pixabay.com/de/daumen-hoch-ordnung-gut-gut-gemacht-2056022/)
+* [life-saver-2407304_1920.jpg](https://pixabay.com/de/lebensretter-wasser-sicherheit-2407304/)
+* [duck-185014_1920.jpg](https://pixabay.com/de/ente-wasserv%C3%B6gel-schwimmen-wasser-185014/)
---
+++ js/automat.js
@@ -0,0 +1,88 @@
+var data;
+var score = 0;
+
+function loadByButtonNumber(oldState, buttonNumber, scoreChange) {
+	let nextState = data[oldState].options[buttonNumber].next;
+	let state = nextState;
+	if (Array.isArray(nextState)) {
+		state = nextState[0].state;
+		for (s of nextState) {
+			if (s.hasOwnProperty("min") && s.hasOwnProperty("max")) {
+				if (s.min <= score && s.max >= score) {
+					state = s.state;
+				}
+			}
+		}
+	}
+	document.getElementById("mainarea").style.opacity = 0;
+	setTimeout(function () {
+		load(state, scoreChange);
+	}, 700);
+}
+
+function load(key, scoreChange) {
+	document.getElementById("mainarea").style.opacity = 1;
+	if (scoreChange === "reset") {
+		score = 0;
+	} else {
+		score += scoreChange;
+	}
+	document.getElementById("background-back").style.backgroundImage = document.getElementById("background-front").style.backgroundImage;
+	document.getElementById("background-front").classList.remove('animate');
+	document.getElementById("background-front").style.opacity = 0;
+	updateElements(key);
+	setTimeout(function () {
+		document.getElementById("background-front").classList.add('animate');
+		document.getElementById("background-front").style.opacity = 1;
+	}, 100);
+}
+
+function updateElements(key) {
+	let state = data[key];
+	recreateButtons(key, state.options);
+	document.getElementById("background-front").style.backgroundImage = "url('images/" + state.image + "')";
+	document.getElementById("text").innerHTML = state.text;
+	document.getElementById("question").innerHTML = state.question;
+}
+
+function recreateButtons(currentState, buttons) {
+	let html = "";
+	let buttonNumber = 0;
+	for (btn of buttons) {
+		let score = wrapScore(btn.score);
+		html += "<button class='" + btn.class + "' onclick='loadByButtonNumber(\"" + currentState + "\", " + buttonNumber + ", " + score + ")'>" + btn.text + "</button> ";
+		buttonNumber++;
+	}
+	document.getElementById("buttons").innerHTML = html;
+}
+
+function wrapScore(score) {
+	if (score === "reset") {
+		return "\"reset\"";
+	} else {
+		return score;
+	}
+}
+
+function preloadImages() {
+	for(let key of Object.keys(data)){
+		let img = document.createElement("img");
+		img.src = "images/" + data[key].image;
+		
+		let src = document.getElementById("preload");
+		src.appendChild(img);
+	}
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			data = JSON.parse(this.responseText);
+			load("start", 0);
+			preloadImages();
+		}
+	};
+	xhttp.open("GET", "data/data.json", true);
+	xhttp.send();
+});
